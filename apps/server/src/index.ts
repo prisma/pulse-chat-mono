20b1a8e9aa50c627437291e6280dbf56528925e7
Sabin Adams <sabintheworld@gmail.com>
2023-08-07 16:02:44 -0700

    MVP

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,11 +1,76 @@
-import express, { Application } from 'express';
-import authRouter from './routes/auth.route';
+import express from 'express';
+import http from 'http';
+import * as socket from "socket.io";
+import prisma from '@pulsechat/db'
+import cors from 'cors'
 
-const app: Application = express();
-const PORT: number = 3001;
+const app = express()
+app.use(cors())
 
-app.use('/auth', authRouter)
-
-app.listen(PORT, (): void => {
-    console.log('SERVER IS UP ON PORT:', PORT);
+const server = http.createServer(app)
+const io = new socket.Server(server, {
+    cors: {
+        origin: true,
+        credentials: true
+    },
 });
+
+const PORT: number = 3001;
+let subscription;
+
+async function main() {
+    // Set up Prisma subscription
+    subscription = await prisma.chat.subscribe({});
+
+    if (subscription instanceof Error) {
+        throw subscription;
+    }
+
+    // Handle socket.io connections
+    io.on('connection', async () => {
+        const posts = await prisma.chat.findMany({
+            select: {
+                message: true,
+                user: {
+                    select: {
+                        name: true
+                    }
+                }
+            },
+            orderBy: {
+                createdAt: 'desc'
+            },
+            take: 30
+        })
+
+        // @ts-ignore
+        io.emit('init-chat', posts.map( post => ({
+            message: post.message,
+            user: post.user.name
+        })))
+        io.on('disconnect', () => {
+            console.log('user disconnected');
+        })
+    })
+
+    // Start the server
+    server.listen(PORT, (): void => {
+        console.log('SERVER IS UP ON PORT:', PORT);
+    });
+
+    // Handle Prisma subscription events
+    for await (const event of subscription) {
+        console.log(event)
+        const user = await prisma.user.findUnique({
+            where: {
+                id: event.after.userId
+            }
+        })
+        io.sockets.emit('chat', {
+            message: event.after.message,
+            user: user.name
+        })
+    }
+}
+
+main()
--- apps/server/src/services/user.service.ts
+++
@@ -1,9 +0,0 @@
-import prisma, { Prisma } from '@pulsechat/db'
-
-export async function createUser(data: Prisma.UserCreateInput) {
-  return prisma.user.create({ data })
-}
-
-export async function findUserByEmail(email: Prisma.UserFieldRefs['email']) {
-  return prisma.user.findUnique({ where: { email } })
-}
