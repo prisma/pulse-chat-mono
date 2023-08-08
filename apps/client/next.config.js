d2a1ebf024a264aea8779f3c3e4d0a0cb22bb676
Sabin Adams <sabintheworld@gmail.com>
2023-08-07 19:55:39 -0700

    Adds next config

--- apps/client/next.config.js
+++ apps/client/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin')
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+        if (isServer) {
+            config.plugins = [...config.plugins, new PrismaPlugin()]
+        }
+
+        return config
+    },
+}
 
 module.exports = nextConfig
