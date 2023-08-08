4737cd310b71f5b9baa65e40bc39fe267e597e7c
Sabin Adams <sabintheworld@gmail.com>
2023-08-07 20:59:09 -0700

    Fixes color

--- apps/client/src/app/components/ChatMessage.tsx
+++ apps/client/src/app/components/ChatMessage.tsx
@@ -3,7 +3,7 @@ import { useSession } from "next-auth/react";
 const ChatMessage = ({ chat }: { chat: { user: string, message: string } }) => {
     const { data: session } = useSession()
 
-    return <div className={`${session?.user.name === chat.user ? 'self-start bg-blue-300 text-left' : 'bg-gray-400 text-right'} max-w-md rounded-3xl bg-gray-300 px-4 py-2 text-xs`}>
+    return <div className={`${session?.user.name === chat.user ? 'self-start bg-blue-300 text-left' : 'bg-gray-400 text-right'} max-w-md rounded-3xl px-4 py-2 text-xs`}>
        <h2 className="font-bold">{chat.user}</h2>
        <p>{chat.message}</p>
     </div>
