16738efdcb12825017f7498588331b024309a2c5
Sabin Adams <sabintheworld@gmail.com>
2023-08-07 19:18:52 -0700

    Updates import casings

--- apps/client/src/app/hooks/useChat.ts
+++ apps/client/src/app/hooks/useChat.ts
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import io, { Socket } from 'Socket.IO-client'
+import io, { Socket } from 'socket.io-client'
 
 const useChat = () => {
     const [_, setSocket] = useState<Socket>()
